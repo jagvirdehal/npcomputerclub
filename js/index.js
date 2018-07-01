0e9f18a3987d9f76bafe9397db420e4ac311d1e9
lagvir <jagvird216@gmail.com>
2018-06-30 21:22:36 -0400

    .

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
 window.setTimeout(function(){
-<<<<<<< HEAD
-<<<<<<< HEAD
   window.location.href = "home/index.html";
-=======
-  window.location.href = "/home/index.html";
->>>>>>> f8a946c09fc4cd0fd5ef2ff05740ae67091cfb85
-=======
-  window.location.href = "/home/index.html";
->>>>>>> f8a946c09fc4cd0fd5ef2ff05740ae67091cfb85
 }, 4000);
