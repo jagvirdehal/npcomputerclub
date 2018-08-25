cb7874d9068d36f714a4baceb97a6176cf703ae7
lagvir <jagvird216@gmail.com>
2018-08-24 23:10:33 -0400

    Commments

--- js/land.js
+++ js/land.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
+  // when the mouse enters an element with the target class, add the grayscale
+  // class to the previous element
   $(".target").mouseenter(function() {
     $(this).prev().addClass("grayscale");
   });
 
+  // remove grayscale class from previous element
   $(".target").mouseleave(function() {
     $(this).prev().removeClass("grayscale");
   });
 
+  // add to previous element of previous element
   $(".target").mouseenter(function() {
     $(this).prev().prev().addClass("grayscale");
   });
 
+  // remove from previos element of previous element
   $(".target").mouseleave(function() {
     $(this).prev().prev().removeClass("grayscale");
   });
